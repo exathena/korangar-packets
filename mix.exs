fb12c12b70678e5ec41bb79aad4da3cad8929020
Alexandre de Souza <alexandre@aledsz.com.br>
2026-01-11 11:17:47 -0300

    add alias to generate rustdoc

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Korangar.MixProject do
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -32,6 +33,14 @@ defmodule Korangar.MixProject do
       {:jason, "~> 1.4.4"},
       {:ecto, "~> 3.13.5", optional: true},
       {:rustler, "~> 0.37.2", github: "rusterlium/rustler", sparse: "rustler_mix", runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      "packets.gen.json": [
+        "cmd cargo rustdoc -p ragnarok-packets -- -Z unstable-options --output-format json"
+      ]
     ]
   end
 end
