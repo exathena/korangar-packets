6a50102ea04173c5aaaa554196475eee0c598a00
Alexandre de Souza <alexandre@aledsz.com.br>
2026-07-26 17:23:22 -0300

    update rustler nif docs

--- native/korangar_packets/src/lib.rs
+++ native/korangar_packets/src/lib.rs
@@ -2,7 +2,7 @@ use rustler::{Binary, Env, Term};
 
 /// Decode given raw bytes to `Packet`.
 ///
-/// Returns `{:ok, packet}` or `{:error, reason}`.
+/// Returns `{:ok, {atom(), Packet.t()}}` or `{:error, term()}`.
 #[rustler::nif]
 fn decode_packet<'a>(env: Env<'a>, data: Binary<'a>) -> Result<Term<'a>, String> {
     unimplemented!()
@@ -10,7 +10,7 @@ fn decode_packet<'a>(env: Env<'a>, data: Binary<'a>) -> Result<Term<'a>, String>
 
 /// Encode the given `Packet` to raw bytes.
 ///
-/// Returns `{:ok, binary}` or `{:error, reason}`.
+/// Returns `{:ok, binary()}` or `{:error, term()}`.
 #[rustler::nif]
 fn encode_packet<'a>(env: Env<'a>, packet: Term<'a>) -> Result<Term<'a>, String> {
     unimplemented!()
