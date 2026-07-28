119f11573e8634b2275a8938ee182b3ed384a41b
Alexandre de Souza <alexandre@aledsz.com.br>
2026-07-28 19:21:05 -0300

    simplify tests

--- test/korangar_packets_test.exs
+++ test/korangar_packets_test.exs
@@ -2,21 +2,13 @@ defmodule KorangarPacketsTest do
   use ExUnit.Case, async: true
   import KorangarPackets.Test
 
-  test "encodes and decodes the login packet" do
-    packet =
-      packet_for(:login_server_login,
-        client_type: 22,
-        name: "aledsz",
-        password: "123456",
-        version: [1, 2, 3, 4]
-      )
-
+  test "encodes and decodes the packet" do
+    packet = packet_for(:login_server_login)
     assert {:ok, bytes} = KorangarPackets.encode_packet(packet)
     assert KorangarPackets.decode_packet(bytes) == {:ok, packet}
   end
 
   test "returns error with invalid packet header" do
-    bytes = <<0, 0, 1, 2, 3, 4>>
-    assert KorangarPackets.decode_packet(bytes) == {:error, "Unknown packet header: 0x00"}
+    assert KorangarPackets.decode_packet(<<0, 0>>) == {:error, "Unknown packet header: 0x00"}
   end
 end
